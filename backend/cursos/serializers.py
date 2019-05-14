3223505353186a1aefb13838bd736e78b4f64ea2
ian07 <ianmazzaglia@gmail.com>
2019-05-14 10:55:51 -0300

    enpoint Clase andanado

--- backend/cursos/serializers.py
+++ backend/cursos/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from cursos.models import Curso
+from cursos.models import Curso, Clase
 
 class CursoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Curso
         fields = ("id", "nombre", "descripcion", "profesores", "alumnos")
+
+
+class ClaseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Clase
+        fields = ("id", "dia", "hora_inicio", "hora_fin", "aula", "curso", "presentes")
--- backend/cursos/urls.py
+++ backend/cursos/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from cursos.views import ListCreateCursosView, CursoDetailView
+from cursos.views import ListCreateCursosView, CursoDetailView, ListCreateClasesView, ClaseDetailView
 
 
 urlpatterns = [
     path('cursos/', ListCreateCursosView.as_view(), name="cursos-list-create"),
     path('cursos/<int:id>/', CursoDetailView.as_view(), name="cursos-detail"),
+    path('clases/', ListCreateClasesView.as_view(), name="clases-list-create"),
+    path('clases/<int:id>/', ClaseDetailView.as_view(), name="clases-detail"),
 ]
