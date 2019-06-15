9d80ee436cef3dfd143d0968454c212f90a0746b
Matias Acosta <matiasacosta1995@gmail.com>
2019-06-15 11:11:21 -0300

    Agregar nombre de usuario al iniciar sesion

--- backend/api_eventos/urls.py
+++ backend/api_eventos/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include, re_path
 from rest_framework_simplejwt import views as jwt_views
+from api_eventos.views import MyTokenObtainPairView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path('api/(?P<version>(v1|v2))/', include('participantes.urls')),
     re_path('api/(?P<version>(v1|v2))/', include('cursos.urls')),
-    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh')
 ]
