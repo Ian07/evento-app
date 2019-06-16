312d7e3f55f2b233868c012a57619c056087ff2f
Matías Acosta <matiasacosta1995@gmail.com>
2019-06-16 16:39:32 -0300

    Cambiar el admin para poder manejar los usuarios - roles

--- backend/participantes/admin.py
+++ backend/participantes/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm
 from participantes.models import (
     Persona, Alumno, Profesor, Disertante, Organizador, Usuario
 )
@@ -19,5 +20,19 @@ admin.site.register(Disertante)
 #Agrego a la admin de Django la posiblidad de ABM objetos del tipo Organizador
 admin.site.register(Organizador)
 
+
+# https://stackoverflow.com/questions/15012235/using-django-auth-useradmin-for-a-custom-user-model
+class UsuarioChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = Usuario
+
+class MyUserAdmin(UserAdmin):
+    form = UsuarioChangeForm
+
+    fieldsets = UserAdmin.fieldsets + (
+            (None, {'fields': ('tipo','persona',)}),
+    )
+
+
 #Agrego a la admin de Django la posiblidad de ABM objetos del tipo Usuario
-admin.site.register(Usuario, UserAdmin)
+admin.site.register(Usuario, MyUserAdmin)
