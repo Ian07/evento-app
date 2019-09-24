e708a272ee6db3fc633cdb460ba9e85e671b9a4d
ian07 <ian_07_tw@hotmail.com>
2019-09-24 19:15:41 -0300

    inscripcion y otros fix

--- frontend/evento_app/src/components/DetalleCurso.js
+++ frontend/evento_app/src/components/DetalleCurso.js
@@ -4,7 +4,7 @@ import CardActions from '@material-ui/core/CardActions'
 import CardContent from '@material-ui/core/CardContent'
 import CardMedia from '@material-ui/core/CardMedia'
 import Typography from '@material-ui/core/Typography'
-import { Link } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import Button from '@material-ui/core/Button'
 
 
@@ -33,6 +33,10 @@ const DetalleCurso = ({match}) => {
 
     const classes = useStyles();
     const [open, setOpen] = React.useState(false);
+    const [open_des, setOpen_des] = React.useState(false);
+    const [cursos, setCursos] = React.useState([]);
+    const [curso, setCurso] = React.useState([]);
+    const [redirect, setRedirect] = React.useState(false);
 
     const handleOpen = () => {
       setOpen(true);
@@ -42,7 +46,13 @@ const DetalleCurso = ({match}) => {
       setOpen(false);
     };  
 
-    const [curso, setCurso] = React.useState([]);
+    const handleOpen_des = () => {
+      setOpen_des(true);
+    };
+
+    const handleClose_des = () => {
+      setOpen_des(false);
+    };  
 
     useEffect(() => {
       fetch(`http://192.168.1.43:8000/api/v1/cursos/${match.params.id}`,{
@@ -55,9 +65,21 @@ const DetalleCurso = ({match}) => {
       .then(json => {
         setCurso(json);
       })
+
+      fetch('http://192.168.1.43:8000/api/v1/alumnos/'+ localStorage.getItem('documento') +'/cursos',{
+        method: 'GET',
+        headers: {
+          Authorization: `JWT ${localStorage.getItem('token')}`,
+        }
+      })
+      .then(res => res.json())
+      .then(json => {
+        setCursos(json);
+      })
+
     }, []);
 
-    const sayHello = () => {
+    const inscribirse = () => {
       fetch(`http://192.168.1.43:8000/api/v1/cursos/${match.params.id}/`,{
         method: 'PUT',
         headers: {
@@ -69,11 +91,37 @@ const DetalleCurso = ({match}) => {
       .then(res => {
         if(res.status === 200){
           alert("Tu inscripcion ah sido registrada!");
+          setOpen(false);
+          setRedirect();
+          window.location.reload(false);
         }
       })
     };
 
-    return(
+    const desinscribirse = () => {
+      fetch(`http://192.168.1.43:8000/api/v1/cursos/${match.params.id}/`,{
+        method: 'PUT',
+        headers: {
+          Authorization: `JWT ${localStorage.getItem('token')}`,
+          'Content-Type': 'application/json'
+        },
+        //body: `{"remove_doc_alumno":${localStorage.getItem('documento')}}`
+        body: `{"remove_doc_alumno":38800940}`
+      })
+      .then(res => {
+        if(res.status === 200){
+          alert("Tu desinscripcion ah sido registrada!");
+          setOpen_des(false);
+          setRedirect();
+          window.location.reload(false);
+        }
+      })
+    };
+
+    if(redirect){
+      return <Redirect to="/"/>
+    }else{
+      return(
         <Card>
             <CardMedia style={{height:0, paddingTop: '56.25%'}}
             image={curso.imagen}
@@ -88,9 +136,15 @@ const DetalleCurso = ({match}) => {
             </Typography>
             </CardContent>
             <CardActions>
-                <button type="button" onClick={handleOpen}>
-                  inscribirse
-                </button>
+                {cursos.find( c => c.id === curso.id)?
+                  <button type="button" onClick={handleOpen_des}>
+                    desinscribirse
+                  </button>
+                :
+                  <button type="button" onClick={handleOpen}>
+                    inscribirse
+                  </button>
+                }
                 <Modal
                   aria-labelledby="transition-modal-title"
                   aria-describedby="transition-modal-description"
@@ -107,15 +161,39 @@ const DetalleCurso = ({match}) => {
                     <div className={classes.paper}>
                       <h2 id="transition-modal-title">inscripción</h2>
                       <p id="transition-modal-description">¿Esta seguro que desea inscribirse en este curso?</p>
-                      <Button size="small" color="primary" onClick={sayHello}>  
+                      <Button size="small" color="primary" onClick={inscribirse}>  
                           Inscribirse
+                      </Button>
+                    </div>
+                  </Fade>
+                </Modal>
+
+                <Modal
+                  aria-labelledby="transition-modal-title"
+                  aria-describedby="transition-modal-description"
+                  className={classes.modal}
+                  open={open_des}
+                  onClose={handleClose}
+                  closeAfterTransition
+                  BackdropComponent={Backdrop}
+                  BackdropProps={{
+                    timeout: 500,
+                  }}
+                >
+                  <Fade in={open_des}>
+                    <div className={classes.paper}>
+                      <h2 id="transition-modal-title">desinscripción</h2>
+                      <p id="transition-modal-description">¿Esta seguro que desea desinscribirse en este curso?</p>
+                      <Button size="small" color="primary" onClick={desinscribirse}>  
+                          Desinscribirse
                       </Button>
                     </div>
                   </Fade>
                 </Modal>
             </CardActions>
         </Card>
-    )
+      )
+    }
 }
 
 export default DetalleCurso
